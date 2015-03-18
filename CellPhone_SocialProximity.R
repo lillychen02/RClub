a6efa741a81990673df297581b141db0510f2818
lillyplant <lillychen02@gmail.com>
2015-03-18 09:23:42 -0700

    Add new file for R Session

---
+++ CellPhone_SocialProximity.R
@@ -0,0 +1,9 @@
+# This R Script will explore my cell phone data 
+#   (1) Discover who I'm communicating with the most
+#   (2) Discover who is communicating with me the most 
+#   (3) Create an index to measure your social proximity 
+#          to a particular contact
+#   (4) Did relationships or communication 
+#             patterns change over time?
+
+#Download all cell phone usage data possible
